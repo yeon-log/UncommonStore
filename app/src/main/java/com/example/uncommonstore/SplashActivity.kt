135ae74df9d600edeb05882f5b6fbe951a05300e
yeon-log <jiyeoncode@gmail.com>
2022-12-02 18:41:35 +0900

    #10 splash 이미지 변경 날짜 : 22.12.02 작업자 : 심지연 작업 내용 : splash 이미지 변경

--- app/src/main/java/com/example/uncommonstore/SplashActivity.kt
+++ app/src/main/java/com/example/uncommonstore/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Handler
 import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
+    // test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
