b0dd7c6a5c924d90eca4a83c0f2a2ed917452577
dudah0776 <dudah0776@gmail.com>
2022-12-03 22:49:29 +0900

    이메일 인증, 구글계정으로 로그인 commit

---
+++ app/src/main/java/com/example/uncommonstore/MyApplication.kt
@@ -0,0 +1,28 @@
+package com.example.uncommonstore
+
+import androidx.multidex.MultiDexApplication
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+
+class MyApplication: MultiDexApplication() {
+    companion object {
+        lateinit var auth: FirebaseAuth
+        var email: String? = null
+
+        fun checkAuth(): Boolean {
+            var currentUser = auth.currentUser
+            return currentUser?.let {
+                email = currentUser.email
+                currentUser.isEmailVerified
+            } ?: let {
+                false
+            }
+        }
+    }
+    override fun onCreate() {
+        super.onCreate()
+        auth = Firebase.auth
+    }
+}
