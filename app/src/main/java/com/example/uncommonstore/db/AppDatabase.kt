d6cab2733c4e0f7cf0752c902e1c7e226ae40b3a
NaDong <63277040+kimnahyeong2@users.noreply.github.com>
2022-12-06 17:00:32 +0900

    #28 EventList 변경 날짜 : 22.12.06 작업자 : 김나형 작업 내용 : 이벤트 리스트 db 정보 변경

--- app/src/main/java/com/example/uncommonstore/db/AppDatabase.kt
+++ app/src/main/java/com/example/uncommonstore/db/AppDatabase.kt
@@ -84,5 +84,12 @@ private val PRODUCT_DATA = arrayListOf(
 
 //room 기본 데이터 세팅
 private val EVENT_DATA = arrayListOf(
-    EventEntity(null, "꿀잠친구_베어춘식이", "https://user-images.githubusercontent.com/63277040/205828117-86c948c2-33c8-4d98-9e20-ad5dbe42ac11.PNG","상품 내용"),
-)
+    EventEntity(null, "망그러진 곰 X 더 현대 서울 언커먼 스토어", "https://user-images.githubusercontent.com/63277040/205828117-86c948c2-33c8-4d98-9e20-ad5dbe42ac11.PNG","행사기간 : 11/4(금)~12/22(목)"),
+    EventEntity(null, "김미묘 X 언커먼스토어", "https://user-images.githubusercontent.com/63277040/205842977-6940e6a8-cd7f-4968-9900-de677cc35145.PNG","9/7(수) Open!"),
+    EventEntity(null, "누누씨 X 언커먼스토어", "https://user-images.githubusercontent.com/63277040/205853086-460599d8-12dd-43dd-9e61-583c9d7dedfe.PNG","7월 15일 오픈!"),
+    EventEntity(null, "여자아이들 팝업스토어", "https://user-images.githubusercontent.com/63277040/205853154-05d0813a-c54d-4c72-bd50-c3270ac3c143.PNG","여자아이들의 미니앨범 5집 I love 발매 기념 팝업스토어가 언커먼스토어에서 진행됩니다."),
+    EventEntity(null, "PLAY HEENDY DAY !", "https://user-images.githubusercontent.com/63277040/205853217-129fdc0a-5a8f-441b-b05e-6ca2cc504465.PNG","흰디 굿즈를 특별한 가격에 만나보세요!\n"),
+    EventEntity(null, "김토끼 스튜도 X 언커먼스토어", "https://user-images.githubusercontent.com/63277040/205853317-5f420c49-1ceb-4655-af7a-f9b7ab5ed61f.PNG","기간 : 6/3 ~ 7/7"),
+    EventEntity(null, "바리수 X 언커먼스토어", "https://user-images.githubusercontent.com/63277040/205853382-f22d236f-267a-4cc8-9a83-112ac0b3fba9.PNG","기간 : 4/22(금) ~ 6/2(목)"),
+    EventEntity(null, "바잇미 팝업스토어", "https://user-images.githubusercontent.com/63277040/205853444-1ff5348d-18e0-4b2e-a12f-c14f1e461927.PNG","일정 : 3/12(토) ~ 4/21(목)")
+    )
